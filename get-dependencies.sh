125c99ddc3551462e3c8466532d63d794547be79
juju4 <juju4@users.noreply.github.com>
2019-11-10 08:43:13 -0500

    add testing with kitchen+docker and vagrant - missing file

---
+++ get-dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+## one script to be used by travis, jenkins, packer...
+
+umask 022
+
+if [ $# != 0 ]; then
+rolesdir=$1
+else
+rolesdir=$(dirname $0)/..
+fi
+
+#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.polarproxy ] && ln -s ansible-polarproxy $rolesdir/juju4.polarproxy
+[ ! -e $rolesdir/juju4.polarproxy ] && cp -R $rolesdir/ansible-polarproxy $rolesdir/juju4.polarproxy
+
+## don't stop build on this script return code
+true
