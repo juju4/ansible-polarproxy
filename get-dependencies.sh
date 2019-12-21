dc8778404b19ed99458584c3514a65f5ef32f226
juju4 <juju4@users.noreply.github.com>
2019-12-21 14:21:10 -0500

    add default-rsyslog test suite

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,6 +10,7 @@ rolesdir=$(dirname $0)/..
 fi
 
 #[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.syslogclient ] && git clone https://github.com/juju4/ansible-syslogclient $rolesdir/juju4.syslogclient
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.polarproxy ] && ln -s ansible-polarproxy $rolesdir/juju4.polarproxy
 [ ! -e $rolesdir/juju4.polarproxy ] && cp -R $rolesdir/ansible-polarproxy $rolesdir/juju4.polarproxy
---
+++ test/integration/default-rsyslog/serverspec/polarproxy_spec.rb
@@ -0,0 +1,63 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+## Use Junit formatter output, supported by jenkins but how to pass environment through kitchen?
+## http://www.agilosoftware.com/blog/configuring-test-kitchen-output-jenkins/
+## https://github.com/test-kitchen/busser-serverspec/issues/9
+if ENV.has_key?('SERVERSPEC_JUNIT')
+  require 'yarjuf'
+  RSpec.configure do |c|
+      c.formatter = 'JUnit'
+  end
+end
+#require 'yarjuf'
+#RSpec.configure do |c|
+#  c.formatter = 'JUnit'
+#end
+
+describe file('/var/_polarproxy/PolarProxy/System.Runtime.dll') do
+  it { should be_file }
+  it { should be_mode 744 }
+  it { should be_owned_by '_polarproxy' }
+end
+describe file('/var/_polarproxy/PolarProxy/PolarProxy') do
+  it { should be_file }
+  it { should be_mode 744 }
+  it { should be_owned_by '_polarproxy' }
+end
+describe file('/etc/systemd/system/PolarProxy.service') do
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by 'root' }
+end
+
+describe port(10443) do
+  it { should be_listening }
+end
+
+describe command('curl --cacert /var/log/PolarProxy/polarproxy.pem -L -D - https://www.google.com'), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
+  let(:sudo_options) { '-u nobody -H' }
+  its(:stdout) { should match /<title>Google<\/title>/ }
+# HTTP/1.1 on Centos7 and Ubuntu 16.04, 2 on Ubuntu 18.04
+  its(:stdout) { should match /HTTP\/.* 200/ }
+  its(:stderr) { should_not match /No such file or directory/ }
+  its(:exit_status) { should eq 0 }
+end
+describe command('curl -k --cacert /var/log/PolarProxy/polarproxy.pem -L -D - https://www.google.com'), :if => os[:family] == 'redhat' do
+  let(:sudo_options) { '-u nobody -H' }
+  its(:stdout) { should match /<title>Google<\/title>/ }
+# HTTP/1.1 on Centos7 and Ubuntu 16.04, 2 on Ubuntu 18.04
+  its(:stdout) { should match /HTTP\/.* 200/ }
+  its(:stderr) { should_not match /No such file or directory/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('curl -k --cacert /var/log/PolarProxy/polarproxy.pem -L -D - https://expired.badssl.com') do
+  let(:sudo_options) { '-u nobody -H' }
+  its(:stdout) { should match /HTTP\/1.1 200 OK/ }
+  its(:stderr) { should_not match /No such file or directory/ }
+#  its(:stderr) { should_not match /OpenSSL SSL_connect: SSL_ERROR_SYSCALL in connection to/ }
+  its(:exit_status) { should eq 0 }
+end
